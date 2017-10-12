f5578260a28d471257a0f775a7c89fe88c39429f
Daniel Klava <klava.daniel@gmail.com>
2017-10-12 20:01:21 -0300

    Parallax update for new layer

--- js/states/preload.js
+++ js/states/preload.js
@@ -8,6 +8,7 @@ Main.Preload.prototype = {
     this.game.load.image('mountains-back', 'assets/images/mountains-back.png');
     this.game.load.image('mountains-mid1', 'assets/images/mountains-mid1.png');
     this.game.load.image('mountains-mid2', 'assets/images/mountains-mid2.png');
+    this.game.load.image('city-far', 'assets/images/city-far.png');
     this.game.load.image('city-front', 'assets/images/city-front.png');
     this.game.load.image('city-mid', 'assets/images/city-mid.png');
     this.game.load.image('sky', 'assets/images/sky.png');
@@ -28,9 +29,11 @@ Main.Preload.prototype = {
     this.image.width = 640;
     this.image.height = 400;
 
-    this.add.tween(this.image).to({ alpha: 1}, 1000, Phaser.Easing.Linear.None, true, 2, 1000, true);
+    //this.add.tween(this.image).to({ alpha: 1}, 1000, Phaser.Easing.Linear.None, true, 2, 1000, true);
 
-    this.time.events.add(Phaser.Timer.SECOND * 1, this.transitionToMenu, this);
+    //this.time.events.add(Phaser.Timer.SECOND * 1, this.transitionToMenu, this);
+
+    this.transitionToMenu();
   },
 
   update : function() {
